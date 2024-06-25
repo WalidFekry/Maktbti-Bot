fcd68120e64bacb8dd68bb2f1813b607093dffb6
Walid <walid_fekry@hotmail.com>
2024-06-26 02:20:16 +0300

    new file:   command/database.js

--- command/index.js
+++ command/index.js
@@ -1,11 +1,13 @@
 import share from './share.js';
 import start from './start.js';
-import info from './info.js';
-export default async function command(client, Markup) {
+import eid_al_adha from './eid_al_adha.js';
+import database from './database.js';
+
+export default async function command(client, Markup, config) {
     try {
         await start(client, Markup);
-        await share(client, Markup);
-        await info(client, Markup);
+        await eid_al_adha(client, config);
+        await database(client, Markup);
     } catch (error) {
         console.log(error);
     }
