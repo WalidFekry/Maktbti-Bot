d16cbb0fb4b8af989c0dde54eb6f50b74a0b9b28
Walid <walid_fekry@hotmail.com>
2024-06-26 03:25:07 +0300

    Edit index.js

--- command/index.js
+++ command/index.js
@@ -1,11 +1,14 @@
 import share from './share.js';
 import start from './start.js';
+import info from './info.js';
 import eid_al_adha from './eid_al_adha.js';
 import database from './database.js';
 
 export default async function command(client, Markup, config) {
     try {
         await start(client, Markup);
+        await share(client, Markup);
+        await info(client, Markup);
         await eid_al_adha(client, config);
         await database(client, Markup);
     } catch (error) {
